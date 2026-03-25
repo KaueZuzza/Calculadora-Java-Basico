47199b3696c363f7d25a2d29fee5f059a092dfb8
KaueZuzza <kauezuzzaw@gmail.com>
2026-03-25 18:14:08 -0300

    Criando uma atividade de poo PetShop atualizacao 2

--- src/main/java/org/example/nivel02_poo/Petshop/Cachorro.java
+++ src/main/java/org/example/nivel02_poo/Petshop/Cachorro.java
@@ -1,9 +1,20 @@
 package org.example.nivel02_poo.Petshop;
 
 public class Cachorro {
-    String nome;
-    String raca;
+    private String nome; //mudando os dois para private
+    private String raca;
 
+    public void setNome(String nome){
+        if (nome.length()>0){
+            this.nome = nome;
+        }else {
+            System.out.println("erro: o nome nao pode estar vazio.");
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
 
     public void latir(){
         System.out.println("au au");
--- src/main/java/org/example/nivel02_poo/Petshop/PetShop.java
+++ src/main/java/org/example/nivel02_poo/Petshop/PetShop.java
@@ -3,11 +3,19 @@ package org.example.nivel02_poo.Petshop;
 public class PetShop {
     public static void main(String[] args){
         Cachorro dog1 = new Cachorro();
-        dog1.nome = "Thor";
+        //dog1.nome = "Thor"; // aqui nao vai funcionar pq e private
+
+        dog1.setNome("Thor");
 
         Cachorro dog2 = new Cachorro();
-        dog2.nome = "Mel";
 
+        //dog2.nome = "Mel";// aqui nao vai funcionar pq e private
+
+        dog2.setNome("Mel");
+        System.out.println("O nome do dog 1 é: " + dog1.getNome());
+        System.out.println("O nome do dog 2 é: " + dog2.getNome());
+
+        System.out.println(" ");
         System.out.print("Thor!, late...");
         dog1.latir();
         System.out.println(" ");
